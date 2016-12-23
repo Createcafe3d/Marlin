2534eb77b3fd9e688a5c300c62f8c3c051f06749
Will Topping <will@createcafe.ca>
2016-12-23 00:18:38 -0600

    Firmware pid values after 10 cycle pid tunes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -314,9 +314,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  #define  DEFAULT_Kp 23.31
+  #define  DEFAULT_Ki 1.82
+  #define  DEFAULT_Kd 74.49
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -358,9 +358,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_bedKp 114.23
+  #define  DEFAULT_bedKi 7.50
+  #define  DEFAULT_bedKd 435.15
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
